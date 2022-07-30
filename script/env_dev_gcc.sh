2414f4f461b729661d2fc1d5405ec1af586b4f04
Alex Robenko <arobenko@gmail.com>
2022-07-30 15:41:31 +1000

    Added appveyor configuration.

---
+++ script/env_dev_gcc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT_DIR=$( dirname ${SCRIPT_DIR} )
+BUILD_DIR="${ROOT_DIR}/build.gcc"
+mkdir -p ${BUILD_DIR}
+cd ${BUILD_DIR}
+
+CC=gcc CXX=g++ cmake .. -DCMAKE_INSTALL_PREFIX=install \
+    -DCMAKE_BUILD_TYPE=Debug -DCC_ASN1_USE_SANITIZERS=ON \
+    -DCC_ASN1_BUILD_UNIT_TESTS=ON -DCC_ASN1_BUILD_APPS=ON -DCC_ASN1_TEST_BUILD_DOC=ON "$@"
